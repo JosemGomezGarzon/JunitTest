8edcdc0e44519640301e2b0ec4768024669fdf3d
JosemGomezGarzon <142318478+JosemGomezGarzon@users.noreply.github.com>
2023-10-19 19:17:56 -0500

    Add files via upload

---
+++ validacionPassword/src/main/java/com/mycompany/validacionpassword/Main.java
@@ -0,0 +1,29 @@
+package com.mycompany.validacionpassword;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Handi
+ */
+public class Main {
+
+    public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese una contraseña: ");
+        String password = scanner.nextLine();
+
+        System.out.println("Confirme la contraseña: ");
+        String confirmPassword = scanner.nextLine();
+
+        try {
+            if (validatePass.validatepassword(password, confirmPassword)) {
+                System.out.println("La contraseña es válida.");
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+    
+}
---
+++ validacionPassword/src/test/java/validarPass.java
@@ -0,0 +1,74 @@
+
+import com.mycompany.validacionpassword.validatePass;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Handi
+ */
+public class validarPass {
+
+    @Test
+    public void testPasswordMatch() {
+        // Test para comprobar que la contraseña coincida
+        assertTrue(validatePass.validatepassword("Abc@1234", "Abc@1234"));
+    }
+
+    @Test
+    public void testPasswordsDoNotMatch() {
+        // Test para comprobar que las contraseñas no coincidan
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, ()
+                -> validatePass.validatepassword("Mismatch1@", "Mismatch2@"));
+        assertEquals("Las contraseñas no coinciden.", exception.getMessage());
+    }
+
+    @Test
+    public void testShortPassword() {
+        // Test para comprabar el tamaño de la contraseña
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, ()
+                -> validatePass.validatepassword("Short1@", "Short1@"));
+        assertEquals("La contraseña debe tener al menos 8 caracteres.", exception.getMessage());
+    }
+
+    @Test
+    public void testPasswordWithSpace() {
+        // Test para comprobar que la contraseña no tenga espacio
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, ()
+                -> validatePass.validatepassword("With Space1@", "With Space1@"));
+        assertEquals("La contraseña no debe contener espacios en blanco.", exception.getMessage());
+    }
+
+    @Test
+    public void testPasswordWithoutLowerCase() {
+        // Test para comprobar que la contraseña no tenga minusculas
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, ()
+                -> validatePass.validatepassword("WITHOUTLOWERCASE1@", "WITHOUTLOWERCASE1@"));
+        assertEquals("La contraseña debe contener al menos un carácter en minúscula, uno en mayúscula, un número y un carácter especial.", exception.getMessage());
+    }
+
+    @Test
+    public void testPasswordWithoutUpperCase() {
+        // Test para comprobar que la contraseña no tenga mayusculas
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, ()
+                -> validatePass.validatepassword("withoutuppercase1@", "withoutuppercase1@"));
+        assertEquals("La contraseña debe contener al menos un carácter en minúscula, uno en mayúscula, un número y un carácter especial.", exception.getMessage());
+    }
+
+    @Test
+    public void testPasswordWithoutDigit() {
+        // Test para comprobar que la contraseña no tenga numeros
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, ()
+                -> validatePass.validatepassword("WithoutDigit@@", "WithoutDigit@@"));
+        assertEquals("La contraseña debe contener al menos un carácter en minúscula, uno en mayúscula, un número y un carácter especial.", exception.getMessage());
+    }
+
+    @Test
+    public void testPasswordWithoutSpecialChar() {
+        // Test para comprobar que la contraseña no tenga un caracter especial
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, ()
+                -> validatePass.validatepassword("WithoutSpecialChar1", "WithoutSpecialChar1"));
+        assertEquals("La contraseña debe contener al menos un carácter en minúscula, uno en mayúscula, un número y un carácter especial.", exception.getMessage());
+    }
+
+}
